fd7717ff12675bbda0b2e3e53fbbaa6f7b1ab291
Joe <joecn2704+github@gmail.com>
2025-06-22 22:50:41 +0400

    deleted unused hook

--- src/hooks/useUnsplash.tsx
+++
@@ -1,9 +0,0 @@
-// import { getPhotosByQuery } from "../api/unsplash";
-// import { useQuery } from "react-query";
-
-// const staleTime = 1000 * 60 * 60 * 2;
-
-// export const useGetPhotosByQuery = ({ query }: { query: string }) =>
-//   useQuery(query, () => getPhotosByQuery({ query }), {
-//     staleTime,
-//   });
